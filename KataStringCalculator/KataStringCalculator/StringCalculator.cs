8728c5731c333a949d8fca98cdafb359fe1b20b1
Jonathan Counihan <jonathan.counihan@entelect.co.za>
2015-05-18 12:04:33 +0200

    First commit for KATA.

---
+++ KataStringCalculator/KataStringCalculator/StringCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KataStringCalculator
+{
+    class StringCalculator
+    {
+        public static int Add(String str)
+        {
+            int value = 0;
+
+            if (!String.IsNullOrEmpty(str))
+            {
+                value = Int32.Parse(str);
+            }
+
+            return value;
+        }
+    }
+}
---
+++ KataStringCalculator/KataStringCalculator/StringCalculatorUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataStringCalculator
+{
+    [TestClass]
+    public class StringCalculatorUnitTests
+    {
+        [TestMethod]
+        public void GivenBlankThen0()
+        {
+            int value = StringCalculator.Add("");
+
+            Assert.AreEqual(0, value);
+        }
+
+        [TestMethod]
+        public void GivenNumberThenNumber()
+        {
+            int value = StringCalculator.Add("10");
+
+            Assert.AreEqual(10, value);
+        }
+    }
+}
