f532baf2f9e7e8cc97f4ccedff01b343ed75e54c
Jonathan Counihan <jonathan.counihan@entelect.co.za>
2015-05-18 21:53:51 +0200

    all basic test cases passing.

--- KataStringCalculator/KataStringCalculator/StringCalculator.cs
+++ KataStringCalculator/KataStringCalculator/StringCalculator.cs
@@ -13,8 +13,24 @@ namespace KataStringCalculator
 
             if (!String.IsNullOrEmpty(str))
             {
-                var strs = str.Split(',');
-                value = strs.Select(x => Int32.Parse(x)).Sum();
+                var delimiter = ",";
+                if (str.StartsWith("//"))
+                {
+                    delimiter = str[2].ToString();
+                    str = str.Substring(2, str.Length - 2);
+                }
+
+                var strs = str.Split(new string[] { delimiter, "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                var ints = strs.Select(x => Int32.Parse(x));
+                var negatives = ints.Where(x => x < 0);
+
+                if (negatives.Count() > 0)
+                {
+                    throw new InvalidOperationException("negatives not allowed " + string.Join(",", negatives));
+                }
+
+                value = ints.Sum();
             }
 
             return value;
--- KataStringCalculator/KataStringCalculator/StringCalculatorUnitTests.cs
+++ KataStringCalculator/KataStringCalculator/StringCalculatorUnitTests.cs
@@ -29,5 +29,42 @@ namespace KataStringCalculator
 
             Assert.AreEqual(21, value);
         }
+
+        [TestMethod]
+        public void GivenLotsOfNumbersThenNumber()
+        {
+            int value = StringCalculator.Add("10,11,14,16");
+
+            Assert.AreEqual(51, value);
+        }
+
+        [TestMethod]
+        public void GivenLotsOfNumbersWithNewLineThenNumber()
+        {
+            int value = StringCalculator.Add("10\n11,14\n16");
+
+            Assert.AreEqual(51, value);
+        }
+
+        [TestMethod]
+        public void GivenDifferentDelimiterThenNumber()
+        {
+            int value = StringCalculator.Add("//;\n1;2");
+
+            Assert.AreEqual(3, value);
+        }
+
+        [TestMethod]
+        public void GivenNegativeNumbersThenException()
+        {
+            try
+            {
+                int value = StringCalculator.Add("//;\n-1;2;-3");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Assert.AreEqual("negatives not allowed -1,-3", ex.Message);
+            }
+        }      
     }
 }
